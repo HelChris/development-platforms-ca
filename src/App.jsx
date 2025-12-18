a4b4509cc4088651d29f8e43b95e0e9b2b8294b2
HelChris <111705707+HelChris@users.noreply.github.com>
2025-12-19 00:21:25 +0100

    fix: trigger new deploy

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import PostPage from "./pages/PostPage.jsx";
 import AddPostPage from "./pages/AddPostPage.jsx";
 import ProfilePage from "./pages/ProfilePage.jsx";
 
+// ..
+
 function App() {
   return (
     <Router>
