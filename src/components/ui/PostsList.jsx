f0361a0d1ed8252a5a5693d6bbd1b8f10b262998
HelChris <111705707+HelChris@users.noreply.github.com>
2025-12-21 23:06:25 +0100

    style: add grid-col-1, md:col-2, lg:col-3

--- src/components/ui/PostsList.jsx
+++ src/components/ui/PostsList.jsx
@@ -89,7 +89,7 @@ function PostsList({ limit = null, showActions = false }) {
 
   return (
     <div className="max-w-7xl mx-auto px-4 py-8">
-      <div className="grid grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {posts.map((post) => (
           <PostCard
             key={post.id}
